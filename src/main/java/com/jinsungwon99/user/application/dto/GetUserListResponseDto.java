e0f206f5766df51a5fad10b5d79bc51728b95076
jinsungwon99 <148827746+jinseongwon99@users.noreply.github.com>
2024-10-12 16:54:40 +0900

    build: QueryDSL 작성
    -Jpa 추가
    -QueryDslConfig 추가

---
+++ src/main/java/com/jinsungwon99/user/application/dto/GetUserListResponseDto.java
@@ -0,0 +1,5 @@
+package com.jinsungwon99.user.application.dto;
+
+public record GetUserListResponseDto (String name, String profileImageUrl){
+
+}
--- src/main/java/com/jinsungwon99/user/application/interfaces/UserRepository.java
+++ src/main/java/com/jinsungwon99/user/application/interfaces/UserRepository.java
@@ -1,10 +1,9 @@
 package com.jinsungwon99.user.application.interfaces;
 
 import com.jinsungwon99.user.domain.User;
-import java.util.Optional;
 
 public interface UserRepository {
 
     User save(User user);
-    Optional<User> findById(Long id);
+    User findById(Long id);
 }
