b294efcf58751acdf9cdadeca061f4eca3b125c0
jinsungwon99 <148827746+jinseongwon99@users.noreply.github.com>
2024-10-21 22:17:53 +0900

    feat: feed 댓글 조회 기능 구현
    - feedController

--- src/main/java/com/jinsungwon99/post/repository/post_queue/UserPostQueueQueryRepository.java
+++ src/main/java/com/jinsungwon99/post/repository/post_queue/UserPostQueueQueryRepository.java
@@ -1,9 +1,12 @@
 package com.jinsungwon99.post.repository.post_queue;
 
+import com.jinsungwon99.post.ui.dto.GetContentResponseDto;
 import com.jinsungwon99.post.ui.dto.GetPostContentResponseDto;
 import java.util.List;
 
 public interface UserPostQueueQueryRepository {
 
     List<GetPostContentResponseDto> getContentResponse(Long userId,Long lastPostId);
+
+    List<GetContentResponseDto> getCommentResponse(Long postId, Long userId, Long lastCommentId);
 }
