93e89e7d840643a4e5e09d09ebfc0d55cbc2ae92
jinsungwon99 <148827746+jinseongwon99@users.noreply.github.com>
2024-10-23 23:20:40 +0900

    test : 인수테스트 템플릿 생성

---
+++ src/test/java/com/jinsungwon99/acceptance/utils/AcceptanceTestTemplate.java
@@ -0,0 +1,24 @@
+package com.jinsungwon99.acceptance.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT) // 기본 8080 포트로 서버 연결
+public class AcceptanceTestTemplate {
+
+    @Autowired
+    private DatabaseCleanUp cleanUp;
+
+    @Autowired
+    private DataLoader loader;
+
+    @BeforeEach
+    public void init(){
+        cleanUp.excute(); // DB 초기화, ID값 초기화
+        loader.loadData(); // 샘플 데이터 주입
+    }
+}
