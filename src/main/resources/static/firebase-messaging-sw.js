a5217cca62c10ad8d1a4ab3d2f340fef4edb43f9
jinsungwon99 <148827746+jinseongwon99@users.noreply.github.com>
2024-11-28 16:40:42 +0900

    feat : FCM 메시징 코드 수정

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -2,8 +2,8 @@ self.addEventListener('push', function(event) {
   const jsonData = event.data.json();
   const options = {
     body: jsonData.data.message,
-    icon: 'favicon.ico',
-    badge: 'favicon.ico'
+    icon: 'logo.jpg',
+    badge: 'logo.jpg'
   };
   event.waitUntil(
     self.registration.showNotification('커뮤니티 피드 서비스 알림', options)
